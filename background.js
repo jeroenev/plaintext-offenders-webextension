1f8747602905c55dc18c9e08c6e606d9a7869779
Jeroen Evens <jeroen7e@gmail.com>
2018-09-11 22:44:48 +0200

    added syncing of data from plaintextoffenders github CSV

--- background.js
+++ background.js
@@ -8,6 +8,25 @@ chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({ color: '#3aa757' }, function () {
     console.log("The color is green.");
   });
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      console.log(xhr.responseText);
+      let offenders_list_raw = xhr.responseText;
+      let offenders_list = offenders_list_raw.split('\n')
+      offenders_list.shift();
+      offenders_list.forEach(function(item) {
+        let key, value; 
+        [key,value] = item.split(',');
+        let obj= {};
+        obj[key] = value;
+        chrome.storage.local.set(obj);
+      });
+    }
+  }
+  xhr.open("GET", "https://raw.githubusercontent.com/plaintextoffenders/plaintextoffenders/master/offenders.csv", true);
+  xhr.send();
+  
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
